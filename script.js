e2c9415ef92da02bb631875b5c3e8ed21ea87dcd
Tamara Naumovic <tamaranaumovic@gmail.com>
2020-01-23 11:26:22 +0100

    razdvojeni delovi po fajlovima

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("submit").addEventListener("click", function() {
+
+    const macadresa = document.getElementById("macadresa").value.toUpperCase();
+    const regex = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+    if (regex.test(macadresa)) {
+        var e = document.getElementById("opcije");
+        var url = "";
+        if (e.options[e.selectedIndex].value == "vibracije") {
+            url = "https://crowdsensing.elab.fon.bg.ac.rs/controller.php?action=vratiMac&mac=" + macadresa;
+        } else if (e.options[e.selectedIndex].value == "buka") {
+            url = "https://crowdsensing.elab.fon.bg.ac.rs/controller.php?action=vratiMacVib&mac=" + macadresa;
+        } else {
+            console.log("Opcija nije odabrana");
+        }
+        $.getJSON(url, function(data) {
+            document.getElementById("brojmerenja").value = data.length;
+            document.getElementById("lblbrojmerenja").hidden = false;
+            document.getElementById("brojmerenja").hidden = false;
+        });
+    } else {
+        alert("Uneta mac adresa nije validna");
+        location.reload();
+    }
+
+});
